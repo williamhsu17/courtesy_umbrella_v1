489e1747ac1dd61fc5e3ea898c00c77d4eb7c346
Fang Ernesto <zxc9799979@gmail.com>
2016-12-28 01:44:09 +0800

    taipei mrt stations csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Location.create!( :name => "Location00")
-Location.create!( :name => "Location01")
-Location.create!( :name => "Location02")
-Location.create!( :name => "Location03")
-Location.create!( :name => "Location04")
+Location.destroy_all
+Umbrella.destroy_all
 
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
-Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => 1)
+Location.create!( :name => "儲存點")
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'taipei-mrt.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  l = Location.new
+  l.name = row['name']
+  l.station_number1 = row['station_number1']
+  l.station_number2 = row['station_number2']
+  l.longitude = row['longitude']
+  l.latitude = row['latitude']
+  l.save
+end
+
+puts "There are now #{Location.count} rows in the locations table"
+
+Location.all.each do |location|
+  10.times do
+    Umbrella.create!( :umbrella_holder_type => "Location", :umbrella_holder_id => location.id)
+  end
+end
