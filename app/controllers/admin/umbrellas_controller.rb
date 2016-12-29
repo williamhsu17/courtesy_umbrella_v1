784fbf9500c96ab4fec9bee923a21e3796a7acc5
williamhsu17 <williamhsu17@gmail.com>
2016-12-29 19:33:20 +0800

    fix N+1 in admin/index

--- app/controllers/admin/umbrellas_controller.rb
+++ app/controllers/admin/umbrellas_controller.rb
@@ -2,26 +2,27 @@ class Admin::UmbrellasController < ApplicationController
   layout 'admin'
 
   def index
-    @locations = Location.all
+    # @locations = Location.all
     @umbrellas = Umbrella.all
-    
-    @umbrellas_in_renting = Umbrella.in_users
-    @umbrellas_in_station = Umbrella.in_stations
-    
-    umbrella_rent_log = RentHistory.all
+    @umbrellas = @umbrellas.includes(:umbrella_holder)
 
-    station_time_log = {}
+    # @umbrellas_in_renting = Umbrella.in_users
+    # @umbrellas_in_station = Umbrella.in_stations
 
-    umbrella_rent_log.each do |u|
-      if u.start_location_id == 1
-        station_time_log[u.start_time] = {:in => u.start_location_id}
-      end
-      if u.end_location_id == 1
-        station_time_log[u.end_time] = {:out => u.end_location_id}
-      end
-    end
+    # umbrella_rent_log = RentHistory.all
 
-    @station_time_log = station_time_log.sort_by { |key, value| key }
+    # station_time_log = {}
+
+    # umbrella_rent_log.each do |u|
+    #   if u.start_location_id == 1
+    #     station_time_log[u.start_time] = {:in => u.start_location_id}
+    #   end
+    #   if u.end_location_id == 1
+    #     station_time_log[u.end_time] = {:out => u.end_location_id}
+    #   end
+    # end
+
+    # @station_time_log = station_time_log.sort_by { |key, value| key }
   end
 
   def show
